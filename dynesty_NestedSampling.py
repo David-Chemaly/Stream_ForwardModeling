573d137d20d1c04752a3d293fd4c7f87b8a4087c
David-Chemaly <davidchemaly@hotmail.com>
2024-01-27 12:59:16 +0000

    first commit

---
+++ dynesty_NestedSampling.py
@@ -0,0 +1,357 @@
+import os
+import h5py
+import pickle
+import dynesty
+import numpy as np
+from tqdm import tqdm
+import astropy.units as u
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from scipy.interpolate import interp1d
+from scipy.stats import norm, truncnorm
+
+from orbit_evolution_potential import run
+
+### Define Data ###
+def generate_data():
+    # 4 for Potential
+    halo_mass     = 7.5e11# * u.Msun 
+    concentration = 20
+    flattening_xy  = 0.75
+    flattening_xz  = 1.25
+
+    # 6 for Initial Conditions
+    pos_init = [50, -30, 40]# * u.kpc 
+    vel_init = [90,100,80]# * u.km/u.s
+
+    # 3 for orientation
+    alpha, beta, charlie = 0.01, -0.5, 0.7
+
+    # 2 for rotation
+    aa, bb = -0.1, 0.3
+
+    # 1 for time
+    t_end = 2.75
+
+    ### Run ###
+    params     = (halo_mass, concentration, flattening_xy, flattening_xz, pos_init[0], pos_init[1], pos_init[2], vel_init[0], vel_init[1], vel_init[2], t_end, alpha, beta, charlie, aa, bb)
+    clean_data = model(params)
+    
+    ### Add Noise ### 
+    sigma = 2
+    dirty_data = clean_data + np.random.normal(0, sigma, clean_data.shape)
+
+    # ### Add Noise only to Radius ### CHEATING ###
+    # sigma = 2
+    # r   = np.sqrt(clean_data[0]**2 + clean_data[1]**2) + np.random.normal(0, sigma, len(clean_data[0]))
+    # phi = np.arctan2(clean_data[1], clean_data[0]) 
+    # dirty_data = np.array([r*np.cos(phi), r*np.sin(phi)])
+
+    return clean_data, dirty_data, sigma
+
+### Define Functions for Dynesty ###
+# Priors
+def prior_transform(utheta):
+    # Unpack the unit cube values
+    u_halo_mass, u_concentration, u_flattening_xy, u_flattening_xz, \
+    u_pos_init_x, u_pos_init_y, u_pos_init_z, u_vel_init_x, u_vel_init_y, u_vel_init_z, \
+    u_t_end, u_alpha, u_beta, u_charlie, u_aa, u_bb  = utheta
+
+    M_min, M_max     = 1e11, 1e12
+    c_min, c_max     = 18, 22
+    qxy_min, qxy_max = 0.5, 1.5
+    qxz_min, qxz_max = 0.5, 1.5
+
+    x_pos_min, x_pos_max = 25, 75
+    y_pos_min, y_pos_max = -75, -25
+    z_pos_min, z_pos_max = 25, 75
+
+    x_vel_min, x_vel_max = 75, 125
+    y_vel_min, y_vel_max = 75, 125
+    z_vel_min, z_vel_max = 75, 125
+
+    t_end_min, t_end_max = 0.5, 1.5
+
+    alpha_mu, alpha_sigma = 0, 1
+    beta_mu, beta_sigma = 0, 1
+    charlie_mu, charlie_sigma = 0, 1
+    aa_mu, aa_sigma = 0, 1
+    bb_mu, bb_sigma = 0, 1
+
+    # Transform each parameter
+    halo_mass     = M_min + u_halo_mass * (M_max - M_min) 
+    concentration = c_min + u_concentration * (c_max - c_min)  
+    flattening_xy = qxy_min + u_flattening_xy * (qxy_max-qxy_min)  
+    flattening_xz = qxz_min + u_flattening_xz * (qxz_max-qxz_min)  
+
+    pos_init_x = x_pos_min + u_pos_init_x * (x_pos_max - x_pos_min) 
+    pos_init_y = y_pos_min + u_pos_init_y * (y_pos_max - y_pos_min) 
+    pos_init_z = z_pos_min + u_pos_init_z * (z_pos_max - z_pos_min) 
+
+    vel_init_x = x_vel_min + u_vel_init_x * (x_vel_max - x_vel_min) 
+    vel_init_y = y_vel_min + u_vel_init_y * (y_vel_max - y_vel_min) 
+    vel_init_z = z_vel_min + u_vel_init_z * (z_vel_max - z_vel_min) 
+
+    t_end = t_end_min + u_t_end * (t_end_max - t_end_min)
+
+    alpha = norm.ppf(u_alpha, loc=alpha_mu, scale=alpha_sigma)
+    beta = norm.ppf(u_beta, loc=beta_mu, scale=beta_sigma)
+    charlie = norm.ppf(u_charlie, loc=charlie_mu, scale=charlie_sigma)
+    aa = norm.ppf(u_aa, loc=aa_mu, scale=aa_sigma)
+    bb = norm.ppf(u_bb, loc=bb_mu, scale=bb_sigma)
+
+    # Return the transformed parameters
+    return (halo_mass, concentration, flattening_xy, flattening_xz,
+            pos_init_x, pos_init_y, pos_init_z, vel_init_x, vel_init_y, vel_init_z,
+            t_end, alpha, beta, charlie, aa, bb)
+
+# Model
+def model(params):
+    # Unpack parameters
+    halo_mass, concentration, flattening_xy, flattening_xz, \
+    pos_init_x, pos_init_y, pos_init_z, vel_init_x, vel_init_y, vel_init_z, \
+    t_end, alpha, beta, charlie, aa, bb = params
+
+    # Repack some of the parameters to match the expected input format of your 'run' function
+    pos_init = np.array([pos_init_x, pos_init_y, pos_init_z]) * u.kpc
+    vel_init = np.array([vel_init_x, vel_init_y, vel_init_z]) * u.km/u.s
+
+    # Call your 'run' function
+    N_time   = 100
+    test_pos = run(halo_mass*u.Msun, concentration, flattening_xy, flattening_xz, pos_init, vel_init, t_end, alpha, beta, charlie, aa, bb, N_time)[:2].value 
+
+    x_pos, y_pos = test_pos[0], test_pos[1]
+
+    gamma_pos = get_gamma(test_pos)
+
+    f_x = interp1d(gamma_pos, x_pos, kind='linear')
+    f_y = interp1d(gamma_pos, y_pos, kind='linear')
+
+    gamma_new = np.linspace(gamma_pos.min(), gamma_pos.max(), test_pos.shape[1])
+
+    x_new = f_x(gamma_new)
+    y_new = f_y(gamma_new)
+
+    return np.array([x_new, y_new])  
+
+
+def get_gamma(pos):
+    x, y = pos[0], pos[1]
+
+    # Calculate differences
+    dx = np.diff(x)
+    dy = np.diff(y)
+
+    # Euclidean distances between successive points
+    distances = np.sqrt(dx**2 + dy**2)
+
+    # Cumulative distance (gamma)
+    gamma = np.cumsum(distances)
+
+    # To include the starting point (distance = 0), you might prepend 0 to gamma
+    gamma = np.insert(gamma, 0, 0)
+
+    return gamma
+
+# Likelihood
+def log_likelihood_gamma(params, dict_data):
+
+    # Unpack the data
+    clean_data  = dict_data['clean_data']
+    dirty_data  = dict_data['dirty_data']
+    sigma = dict_data['sigma']
+
+    # Generate model predictions for the given parameters
+    test_pos = model(params)
+
+    data_gamma = get_gamma(clean_data) ### CHEATING BY USING CLEAN DATA ###
+    test_gamma = get_gamma(test_pos)
+    f_x = interp1d(test_gamma, test_pos[0], kind='linear')
+    f_y = interp1d(test_gamma, test_pos[1], kind='linear')
+
+    # Outlier (something really big)
+    outlier = 1e2 #1e8
+
+    test_pred = np.zeros(dirty_data.shape)
+    if test_gamma[-1] < data_gamma[-1]:
+
+        len_in    = np.sum(data_gamma < test_gamma[-1])
+        test_pred[0,:len_in] = f_x(data_gamma[:len_in])
+        test_pred[1,:len_in] = f_y(data_gamma[:len_in])
+
+        # Compute residuals after fit and punishement for being too short
+        residuals   = dirty_data[:, :len_in] - test_pred[:,:len_in]
+        punishement = outlier * (data_gamma[-1] - test_gamma[-1])
+
+    elif test_gamma[-1] > data_gamma[-1]:
+
+        test_pred[0] = f_x(data_gamma)
+        test_pred[1] = f_y(data_gamma)
+
+        # Compute residuals after fit and punishement for going beyond data
+        residuals   = dirty_data - test_pred
+        punishement = outlier * (test_gamma[-1] - data_gamma[-1])
+
+    else:
+        # Compute residuals after fit
+        residuals   = dirty_data - test_pos
+        punishement = 0
+
+    # Assuming 'sigma' is either a scalar or an array of standard deviations for your observed data
+    # Chi-squared statistic
+    chi_squared = np.sum((residuals / sigma) ** 2)
+
+    # Convert chi-squared to log-likelihood
+    # Note: This assumes Gaussian errors. Modify if your error distribution is different.
+    log_likelihood = -0.5 * chi_squared - punishement
+    
+    return log_likelihood
+
+def log_likelihood_density(params, dict_data):
+
+    # Unpack the data
+    clean_data  = dict_data['clean_data']
+    dirty_data  = dict_data['dirty_data']
+    sigma = dict_data['sigma']
+
+    # Generate model predictions for the given parameters
+    model_data = model(params)
+
+    # Based on FOV of data
+    x_min, x_max = -120, -20
+    y_min, y_max = 10, 100
+
+    # Based on spatial resolution
+    reso = 30
+
+    im_data  = np.histogram2d(dirty_data[0], dirty_data[1], bins=reso, range=[[x_min, x_max], [y_min, y_max]])[0]
+    im_model = np.histogram2d(model_data[0], model_data[1], bins=reso, range=[[x_min, x_max], [y_min, y_max]])[0]
+
+    if im_model.sum() != im_data.sum():
+        log_likelihood = -np.inf
+
+    else:
+        # Assuming 'sigma' is either a scalar or an array of standard deviations for your observed data
+        # Chi-squared statistic
+        chi_squared = np.sum(((im_data - im_model) / sigma) ** 2)
+
+        # Convert chi-squared to log-likelihood
+        # Note: This assumes Gaussian errors. Modify if your error distribution is different.
+        log_likelihood = -0.5 * chi_squared
+    
+    return log_likelihood
+
+def log_likelihood_phi(params, dict_data):
+
+    # Unpack the data
+    clean_data  = dict_data['clean_data']
+    dirty_data  = dict_data['dirty_data']
+    sigma = dict_data['sigma']
+
+    # Generate model predictions for the given parameters
+    model_data = model(params)
+
+    r_data   = np.sqrt(dirty_data[0]**2 + dirty_data[1]**2)
+    phi_data = np.arctan2(dirty_data[1], dirty_data[0])
+
+    r_model   = np.sqrt(model_data[0]**2 + model_data[1]**2)
+    phi_model = np.arctan2(model_data[1], model_data[0])
+
+    # Based on FOV of data
+    padding = 0.01
+    sign_min, sign_max = phi_data.min()/abs(phi_data.min()), phi_data.max()/abs(phi_data.max())
+
+    if sign_min < 0:
+        phi_min_limit = phi_data.min()*(1+padding)
+    elif sign_min > 0:
+        phi_min_limit = phi_data.min()*(1-padding)
+    else:
+        phi_min_limit = phi_data.min()
+    if sign_max < 0:
+        phi_max_limit = phi_data.max()*(1-padding)
+    elif sign_max > 0:
+        phi_max_limit = phi_data.max()*(1+padding)
+    else:
+        phi_max_limit = phi_data.max()
+
+    arg_in  = np.where( (phi_model.min() <= phi_data) &  (phi_data <= phi_model.max()) )[0]
+    arg_out = np.where( (phi_model <= phi_min_limit) |  (phi_model >= phi_max_limit) )[0]
+
+    if len(arg_out) != 0: # or len(arg_in) != len(phi_data):
+        log_likelihood = -np.inf
+
+    else:
+        fill = 0
+        f_r   = interp1d(phi_model, r_model, kind='linear', bounds_error=False, fill_value=fill)
+        r_new = f_r(phi_data)
+
+        arg_out = np.where(r_new == 0)[0]
+        arg_bot = arg_out[np.where(arg_out < len(phi_data)//2)[0]]
+        arg_top = arg_out[np.where(arg_out > len(phi_data)//2)[0]]
+        for i in np.flip(arg_bot):
+            r_new[i] = r_new[i+1]
+        for i in arg_top:
+            r_new[i] = r_new[i-1]
+
+        # Assuming 'sigma' is either a scalar or an array of standard deviations for your observed data
+        # Chi-squared statistic
+        chi_squared = np.sum(((r_data - r_new) / sigma) ** 2)
+
+        # Convert chi-squared to log-likelihood
+        # Note: This assumes Gaussian errors. Modify if your error distribution is different.
+        log_likelihood = -0.5 * chi_squared
+    
+    return log_likelihood
+
+def log_likelihood_prob(params, dict_data):
+
+    # Unpack the data
+    clean_data  = dict_data['clean_data']
+    dirty_data  = dict_data['dirty_data']
+    sigma = dict_data['sigma']
+
+    # Generate model predictions for the given parameters
+    model_data = model(params)
+    length = get_gamma(model_data)[-1]
+
+    # Probability of CDF
+    dx, dy = 0.01, 0.01
+
+    x_min = norm.cdf(model_data[0][None, :]-dx, dirty_data[0][:, None], sigma)
+    x_max = norm.cdf(model_data[0][None, :]+dx, dirty_data[0][:, None], sigma)
+
+    y_min = norm.cdf(model_data[1][None, :]-dy, dirty_data[1][:, None], sigma)
+    y_max = norm.cdf(model_data[1][None, :]+dy, dirty_data[1][:, None], sigma)
+    
+    x_prob = np.sum( abs(x_max - x_min), axis=1)
+    y_prob = np.sum( abs(y_max - y_min), axis=1)
+
+    # Log likelihood
+    if np.where(x_prob == 0)[0].size != 0 or np.where(x_prob == 0)[0].size != 0:
+        log_likelihood = -np.inf
+    else:
+        log_likelihood = np.sum(np.log(x_prob) + np.log(y_prob))# - length
+
+    return log_likelihood    
+
+
+if __name__ == "__main__":
+
+    ### Generate Data ###
+    clean_data, dirty_data, sigma = generate_data()
+    dict_data   = {'clean_data': clean_data, 'dirty_data': dirty_data , 'sigma': sigma}
+
+    ### Run Dynesty ###
+    nworkers = os.cpu_count()
+    pool = Pool(nworkers)
+
+    ndim    = 16  # Number of dimensions (parameters)
+    sampler = dynesty.NestedSampler(log_likelihood_prob, prior_transform, ndim, pool=pool, queue_size=nworkers, logl_args=[dict_data])
+
+    sampler.run_nested()
+    pool.close()
+    pool.join()
+
+    results = sampler.results
+    with open('./dynesty_results_Nt100_prob_punishLength.pkl', 'wb') as file:
+        pickle.dump(results, file)
---
+++ orbit_evolution_potential.py
@@ -0,0 +1,241 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import astropy.units as u
+from tqdm import tqdm
+from scipy.spatial.transform import Rotation
+
+from astropy.constants import G
+from astropy.cosmology import default_cosmology
+cosmo = default_cosmology.get()
+rho_c = (3 * cosmo.H(0.0) ** 2 / (8 * np.pi * G)).to(u.Msun / u.kpc ** 3)
+
+class NFW():
+
+    def __init__(self,M,c,qxy,qxz):
+        self.M = M
+        self.c = c
+        self.qxy = qxy
+        self.qxz = qxz
+
+    def radius_flatten(self,x,y,z):
+        return np.sqrt((x)**2+(y/self.qxy)**2+(z/self.qxz)**2)
+    
+    def A_NFW(self):
+        return np.log(1+self.c) - self.c/(1+self.c)
+    
+    def rho0_Rscube(self):
+        return self.M/self.A_NFW()
+    
+    # Convention 200
+    def Rvir_fct_M(self):
+        return (3*self.M/(4*np.pi*200*rho_c))**(1/3) 
+    
+    def Rs_fct_RvirAndc(self):
+        return self.Rvir_fct_M()/self.c
+        
+    # Outputs potential in (km/s)^2
+    def potential(self,x,y,z):
+        r  = self.radius_flatten(x,y,z)
+
+        Rs = self.Rs_fct_RvirAndc()
+
+        return - G/r * self.M/self.A_NFW() * np.log(1 + r/Rs)
+    
+    # Outputs acceleration in km/s^2
+    def acceleration(self,x,y,z):
+        r   = self.radius_flatten(x,y,z)
+        Rs  = self.Rs_fct_RvirAndc()
+
+        a_r = -G*self.M/(self.A_NFW()*r**2*(r+Rs)) * ( (r+Rs)*np.log(1 + r/Rs) - r)
+
+        a_x = a_r/r * x
+        a_y = a_r/r * y/self.qxy
+        a_z = a_r/r * z/self.qxz
+
+        return [a_x.value,a_y.value,a_z.value] * a_x.unit
+    
+    # Outputs second derivative of the potential in 1/s^2
+    def second_derivative_potential(self,x,y,z):
+        r   = self.radius_flatten(x,y,z)
+        Rs  = self.Rs_fct_RvirAndc()
+
+        factor = G*self.M/(self.A_NFW()*r**3*(r+Rs)**2)
+
+        return factor * ( r*(2*Rs+3*r) - 2*(Rs+r)**2*np.log(1+r/Rs) )
+            
+    def mass_enclosed(self,r):
+        return self.M * (np.log(1 + r/self.Rs_fct_RvirAndc()) - r/(r + self.Rs_fct_RvirAndc()))
+    
+def LeepFrog(a_fct, x_old, y_old, z_old, vx_old, vy_old, vz_old, dt):
+
+    acc_old = a_fct(x_old,y_old,z_old)
+
+    acc_x_old = acc_old[0].to(u.km/u.s**2)
+    acc_y_old = acc_old[1].to(u.km/u.s**2)
+    acc_z_old = acc_old[2].to(u.km/u.s**2)
+    
+    vx_half = vx_old + 0.5*dt*acc_x_old
+    vy_half = vy_old + 0.5*dt*acc_y_old
+    vz_half = vz_old + 0.5*dt*acc_z_old
+
+    x_new = x_old + dt*vx_half
+    y_new = y_old + dt*vy_half
+    z_new = z_old + dt*vz_half
+
+    acc_new = a_fct(x_new,y_new,z_new)
+
+    acc_x_new = acc_new[0].to(u.km/u.s**2)
+    acc_y_new = acc_new[1].to(u.km/u.s**2)
+    acc_z_new = acc_new[2].to(u.km/u.s**2)
+
+    vx_new = vx_half + 0.5*dt*acc_x_new
+    vy_new = vy_half + 0.5*dt*acc_y_new
+    vz_new = vz_half + 0.5*dt*acc_z_new
+
+    return x_new, y_new, z_new, vx_new, vy_new, vz_new
+
+def Rx(theta):
+    """Returns the 3D rotation matrix for a rotation around the X-axis by an angle theta (in radians)."""
+    return np.array([
+        [1, 0, 0],
+        [0, np.cos(theta), -np.sin(theta)],
+        [0, np.sin(theta), np.cos(theta)]
+    ])
+
+def Ry(theta):
+    """Returns the 3D rotation matrix for a rotation around the Y-axis by an angle theta (in radians)."""
+    return np.array([
+        [np.cos(theta), 0, np.sin(theta)],
+        [0, 1, 0],
+        [-np.sin(theta), 0, np.cos(theta)]
+    ])
+
+def Rz(theta):
+    """Returns the 3D rotation matrix for a rotation around the Z-axis by an angle theta (in radians)."""
+    return np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+
+def uniform_random_rotation(data, theta):
+    """Apply a random rotation in 3D, with a distribution uniform over the
+    sphere.
+
+    Arguments:
+        data: vector or set of vectors with dimension (3, n), where n is the
+            number of vectors
+
+        theta: uniformal random variable in [0, 1)
+
+    Returns:
+        Array of shape (3, n) containing the randomly rotated vectors of x,
+        about the mean coordinate of x.
+
+    Algorithm taken from "Fast Random Rotation Matrices" (James Avro, 1992):
+    https://doi.org/10.1016/B978-0-08-050755-2.50034-8
+    """
+
+    def generate_random_z_axis_rotation(x1):
+        """Generate random rotation matrix about the z axis."""
+        R = np.eye(3)
+        # x1 = np.random.rand()
+        R[0, 0] = R[1, 1] = np.cos(2 * np.pi * x1)
+        R[0, 1] = -np.sin(2 * np.pi * x1)
+        R[1, 0] = np.sin(2 * np.pi * x1)
+        return R
+
+    # Transpose from (3, n) to (n, 3)
+    x = data.T
+
+    # There are two random variables in [0, 1) here (naming is same as paper)
+    x1 = theta[0] #np.random.rand()
+    x2 = 2 * np.pi * theta[1] #np.random.rand()
+    x3 = theta[2] #np.random.rand()
+
+    # Rotation of all points around x axis using matrix
+    R = generate_random_z_axis_rotation(x1)
+    v = np.array([
+        np.cos(x2) * np.sqrt(x3),
+        np.sin(x2) * np.sqrt(x3),
+        np.sqrt(1 - x3)
+    ])
+    H = np.eye(3) - (2 * np.outer(v, v))
+    M = -(H @ R)
+    x = x.reshape((-1, 3))
+    mean_coord = np.mean(x, axis=0)
+    x_rot = ((x - mean_coord) @ M) + mean_coord @ M
+    data_rot = x_rot.T
+    return data_rot
+
+def quaternion_to_rotation_matrix(q):
+    w, x, y, z = q
+    return np.array([
+        [1 - 2*y*y - 2*z*z,     2*x*y - 2*z*w,       2*x*z + 2*y*w],
+        [2*x*y + 2*z*w,         1 - 2*x*x - 2*z*z,   2*y*z - 2*x*w],
+        [2*x*z - 2*y*w,         2*y*z + 2*x*w,       1 - 2*x*x - 2*y*y]
+    ])
+
+class get_mat():
+
+    def __init__(self, a, b, c, aa, bb):
+
+        self.v1 = np.array([0, 0, 1])
+
+        v2 = np.array([a, b, c])
+        self.v2 = v2 / np.sum(v2**2)**.5
+
+        v3 = np.cross(self.v1, self.v2)
+        self.v3 = v3 / np.sum(v3**2)**.5
+
+        self.aa = aa
+        self.bb = bb
+    
+    def orientation(self):
+        
+        angle = np.arccos(np.sum(self.v1 * self.v2))
+
+        return Rotation.from_rotvec(angle * self.v3).as_matrix()
+    
+    def rotation(self):
+
+        z_new = - (self.aa * self.v2[0] + self.bb * self.v2[1]) / self.v2[2]
+        v_new = np.array([self.aa, self.bb, z_new])
+
+        v_norm = v_new / np.sum(v_new**2)**.5
+
+        new_angle = np.arccos(np.sum(v_norm*self.v3)) 
+
+        return Rotation.from_rotvec(new_angle * self.v2).as_matrix()
+    
+def run(Mass, concentraion, qxy, qxz, pos_init, vel_init, t_end, alpha, beta, gama, aa, bb, N_time):
+
+    # Define Potential
+    halo = NFW(Mass, concentraion, qxy, qxz)
+
+    # Define Initial Conditions
+    xp, yp, zp    = pos_init[0], pos_init[1], pos_init[2]
+    vxp, vyp, vzp = vel_init[0], vel_init[1], vel_init[2]
+
+    # Define Time
+    time = np.linspace(0, t_end, N_time) # * u.Gyr
+    dt = time[1] - time[0]
+
+    # Initialize Arrays
+    all_pos_p = np.zeros([3, len(time)+1]) * u.kpc
+    all_pos_p[:,0] = [xp,yp,zp] 
+
+    # Evolve Orbit
+    for tndex, t in enumerate(time):
+        xp, yp, zp, vxp, vyp, vzp = LeepFrog(halo.acceleration, xp, yp, zp, vxp, vyp, vzp, dt*u.Gyr)
+        all_pos_p[:,int(tndex+1)] = [xp,yp,zp]
+    
+    # Rotate
+    rot_mat = get_mat(alpha, beta, gama, aa, bb)
+
+    R_orientation = rot_mat.orientation()
+    R_rotation    = rot_mat.rotation()
+
+    all_pos_p = R_rotation  @ R_orientation @ all_pos_p
+
+    return all_pos_p
